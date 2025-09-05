f82c3002725ec7d085b7af07d40038bb67d90395
triyeTechnologies <triye3@gmail.com>
2025-09-06 03:41:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/triye-website/',
-  server: {
-    host: true,
-    port: 3000,
-  },
   build: {
     outDir: 'dist',
     sourcemap: true,
