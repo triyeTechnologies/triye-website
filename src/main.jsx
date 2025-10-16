52928d66a485d09efb350bf512239f2dade51449
triyeTechnologies <triye3@gmail.com>
2025-10-16 14:26:39 +0530

    Add React Router for admin page routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import AdminPage from './components/AdminPage.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
